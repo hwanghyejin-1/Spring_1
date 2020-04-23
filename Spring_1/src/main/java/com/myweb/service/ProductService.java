57313689b8aecb4240445e361f99b4b6182ccc6b
hwanghyejin-1 <hhjin1025@naver.com>
2020-04-23 20:16:08 +0900

    spring

---
+++ Spring_1/src/main/java/com/myweb/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.myweb.service;
+// Service는 DAO가 DB에서 받아온 데이터를 전달받아 가공하는 것
+import java.util.List;
+
+import com.myweb.domain.ProductVO;
+
+public interface ProductService {
+	public void write(ProductVO pvo);
+	public List<ProductVO> list();
+	public ProductVO detail(Integer pno);
+	public void modify(ProductVO pvo);
+	public void remove(Integer pno);
+	
+}
